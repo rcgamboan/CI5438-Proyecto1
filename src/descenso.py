b2e4ad0c19b30170e0b960395d5255b00d9c615d
rcgamboan <rcgamboan@gmail.com>
2023-10-30 19:41:49 -0400

    mejora descenso de gradiente

--- src/descenso.py
+++ src/descenso.py
@@ -1,105 +1,115 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-import random
 
-def gradient_descent(X, Y, theta, alpha, iteraciones):
+def calcular_costo(X, y, theta):
+    m = y.size
+    hipotesis = np.dot(X, theta.T)
+    error = hipotesis - y
+    costo = np.sum((hipotesis-y)**2) / m
+    
+    return costo, error
+
+def gradient_descent(X, y, theta, alpha, iters,tolerancia = 1e-6):
     """
     Implementa el algoritmo de descenso de gradiente para regresión lineal multivariada.
 
     Args:
-        X: Matriz de datos de entrada.
-        Y: Matriz de datos de salida.
+        X: Matriz con los datos de las caracteristicas.
+        y: Matriz de datos de salida.
         theta: Vector de parámetros iniciales.
         alpha: Tasa de aprendizaje.
         iteraciones: Número de iteraciones.
+        tolerancia: Tolerancia para la convergencia.
 
     Returns:
-        Vector de parámetros optimizados.
+        theta: Vector theta con los pesos de la hipotesis.
+        cost_array: Arreglo con los costos de cada iteracion.
+        iters: Numero de iteraciones realizadas antes de finalizar el algoritmo.
     """
-    costos = []
-
-    for iter in range(iteraciones):
-        
-        y_hat = np.dot(X, theta)
-        loss = (y_hat - Y)**2
-        # loss = (Y - y_hat)**2     Revisar si es y - y_hat o y_hat - y
-
-        gradient = X.T.dot(y_hat - Y) * 2/X.shape[0]
-        # gradient = X.T.dot(Y - y_hat) * 2/X.shape[0]  Revisar si es y - y_hat o y_hat - y
-
-        theta -= (alpha * gradient)
-        cost_value = 1/(2*len(Y))*(loss) # Calcular costo de la iteracion
-        total = np.sum(cost_value)
-        costos.append(total)
     
-    plt.plot(np.arange(1,iteraciones),costos[1:], color = 'red')
-    plt.title('Costo vs # de iteracion')
-    plt.xlabel('Iteracion #')
-    plt.ylabel('Costo')
+    cost_array = np.zeros(iters)
+    m = y.size
+    costo_ant = None
+
+    for i in range(iters):
+
+        costo_act, error = calcular_costo(X, y, theta)
+
+        # Convergencia
+        if costo_ant and abs(costo_ant-costo_act)<=tolerancia:
+            print(f"Convergencia en iteracion {i}\n")
+            iters = i
+            break
+
+        costo_ant = costo_act
+        grad = np.dot(X.T, error)
+        theta -= (alpha * (1/m) * grad)
+        cost_array[i] = costo_act
+
+    return theta, cost_array, iters
+
+def plotChart(iterations, cost_num):
+    fig, ax = plt.subplots()
+    ax.plot(np.arange(iterations), cost_num, 'r')
+    ax.set_xlabel('Iteracion #')
+    ax.set_ylabel('Costo')
+    ax.set_title('Costo vs # Iteracion')
+    plt.style.use('fivethirtyeight')
     plt.show()
-    
-    return theta
-
-
-def generar_datos(cant_puntos, bias, variance, cant_variables):
-    x = np.zeros(shape=(cant_puntos, cant_variables))
-    y = np.zeros(shape=cant_puntos)
-    # basically a straight line
-    for i in range(0, cant_puntos):
-        for j in range(cant_variables):
-            x[i][j] = random.uniform(0, 1) * variance + i
-            y[i] = (i + bias) + random.uniform(0, 1) * variance
-    return x, y
 
 def main():
-    # Datos de entrenamiento
-    # X = np.array([[1, 2], [3, 4], [5, 6]])
-    # y = np.array([3, 7, 11])
 
-    x, y = generar_datos(5, 25, 10,2)
+    # Import data
+    data = pd.read_csv('../doc/house_practice.csv')
 
-    # Parámetros iniciales
-    hipotesis_inicial = np.ones(x.shape[1])
+    # Extract data into X and y
+    X = data[['Size', 'Bedrooms']]
+    y = data['Price']
 
-    # Tasa de aprendizaje
-    alpha = 0.0001
+    # Se normalizan las caracteristicas X
+    # para evitar que las características con valores más grandes 
+    # tengan un impacto desproporcionado en el proceso de aprendizaje
 
-    # Número de iteraciones
-    iteraciones = 1000
+    # Si las características tienen valores muy diferentes, 
+    # el gradiente puede ser muy sensible a las características con valores más grandes. 
+    # Esto puede provocar que el modelo se ajuste demasiado a estas características y 
+    # que se vea afectado por el ruido.
+    X = (X - X.mean()) / X.std()
 
-    # Entrenamiento del modelo
-    hipotesis = gradient_descent(x, y, hipotesis_inicial, alpha, iteraciones)
+    # Se le agrega otra columna a X con 1 para permitir el descenso de gradiente vectorizado
+    # En lugar de calcular el gradiente para cada característica individualmente, 
+    # el descenso de gradiente vectorizado calcula el gradiente para todas las características a la vez.
+    X = np.c_[np.ones(X.shape[0]), X] 
 
-    #print(f"X: {x}")
-    #print(f"y: {y}")
-    print(f"hipotesis: {hipotesis}")
 
-    """
-    # Initialize layout
-    fig, ax = plt.subplots(figsize = (9, 9))
+    # Tasa de aprendizaje y numero de iteraciones
+    # Si la tasa de aprendizaje es demasiado alta, 
+    # el modelo puede oscilar entre diferentes mínimos locales y no converjer a un minimo global. 
+    # Si la tasa de aprendizaje es demasiado baja, el modelo puede tardar mucho tiempo en converger. 
+    # En algunos casos, el modelo puede no converger en absoluto.
+    alpha = 0.0005
+    iterations = 10000
 
-    # Add scatterplot
-    ax.scatter(x, y, s=60, alpha=0.7, edgecolors="k")
+    # Inicializar vector theta
+    theta = np.zeros(X.shape[1])
 
-    # Fit linear regression via least squares with numpy.polyfit
-    # It returns an slope (b) and intercept (a)
-    # deg=1 means linear fit (i.e. polynomial of degree 1)
-    b = hipotesis[0]
-    a = hipotesis[1]
+    # Ejecutar descenso de gradiente
+    # al ejecutar el algoritmo, se obtiene una ecuacion de la forma
+    # y = theta_0 + theta_1 * x_1 + theta_2 * x_2
+    # donde:
+    #   y es lo que se quiere predecir
+    #   theta_0 es el término independiente
+    #   theta_1 es el coeficiente de la caracteristica x1
+    #   theta_2 es el coeficiente de la característica x2
+    theta, lista_costos, iters = gradient_descent(X, y, theta, alpha, iterations)
 
-    # Create sequence of 100 numbers from 0 to 100 
-    xseq = np.linspace(0, 10, num=100)
+    # Mostrar grafica de costo vs iteraciones
+    plotChart(iters, lista_costos[0:iters])
 
-    # Plot regression line
-    ax.plot(xseq, a + b * xseq, color="k", lw=2.5)
+    costo_final, _ = calcular_costo(X, y, theta)
 
-    # Set title and labels
-    ax.set_title("Regresión lineal")
-    ax.set_xlabel("X")
-    ax.set_ylabel("y")
-    ax.grid(True)
-    ax.show()"""
-
+    print(f"Costo final: {costo_final}\ntheta final: {theta}\n")
 
 if __name__ == "__main__":
     main()
