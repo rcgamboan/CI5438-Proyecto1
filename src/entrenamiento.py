d72cc5ca9ed45884bbb89ef0ab01156edbd06523
luiss-23 <lblancog23@gmail.com>
2023-11-02 13:52:12 -0400

    training update

--- src/entrenamiento.py
+++ src/entrenamiento.py
@@ -7,11 +7,8 @@ import numpy as np
 
 def main():
 
-    # Limpiar data
-    clean_data('../doc/CarDekho.csv', 2)
-
-    # Cargar data
-    data = pd.read_csv('../doc/CarDekho_clean.csv')
+    # Cargar y limpiar data
+    data, precios_reales = clean_data('doc/CarDekho.csv', 2)
 
     # Separar data de entrenamiento y de prueba
     # se utiliza un 80% para entrenamiento y 20% para prueba
@@ -19,13 +16,19 @@ def main():
     train = data.iloc[:sep,:]
     test = data.iloc[sep:,:]
 
+    # Separando los precios reales de los conjuntos de entrenamiento y prueba
+    max_price = precios_reales.max()
+    min_price = precios_reales.min()
+    precios_reales_train = precios_reales.iloc[:sep]
+    precios_reales_test = precios_reales.iloc[sep:]
+
     # Obtener columna Price para y
     y = train['Price']
 
     # Filtrar columnas para X
     X = train.drop('Price', axis=1)
 
-    print("Datos cargados")
+    print("Datos entrenamiento cargados")
     print(f"X: {X}")
     print(f"y: {y}")
 
@@ -42,16 +45,16 @@ def main():
     # Se le agrega otra columna a X con 1 para permitir el descenso de gradiente vectorizado
     # En lugar de calcular el gradiente para cada característica individualmente, 
     # el descenso de gradiente vectorizado calcula el gradiente para todas las características a la vez.
-    X = np.c_[np.ones(X.shape[0]), X] 
+    X = np.c_[np.ones(X.shape[0]), X]
 
 
     # Tasa de aprendizaje y numero de iteraciones
     # Si la tasa de aprendizaje es demasiado alta, 
-    # el modelo puede oscilar entre diferentes mínimos locales y no converjer a un minimo global. 
+    # el modelo puede oscilar entre diferentes mínimos locales y no converger a un minimo global. 
     # Si la tasa de aprendizaje es demasiado baja, el modelo puede tardar mucho tiempo en converger. 
     # En algunos casos, el modelo puede no converger en absoluto.
-    alpha = 0.0005
-    iterations = 10000
+    alpha = 0.05
+    iterations = 100000
 
     # Inicializar vector theta
     theta = np.zeros(X.shape[1])
@@ -72,7 +75,6 @@ def main():
     plotChart(iters, lista_costos[0:iters])
 
     costo_final, _ = calcular_costo(X, y, theta)
-
     print(f"Costo final: {costo_final}\ntheta final: {theta}\n")
 
     # Predecir precios de la data de prueba
@@ -86,6 +88,14 @@ def main():
 
     # Imprimir RMSE
     print(f"RMSE: {rmse}")
+
+    # Desnormalizar precios
+    for price in range(len(y_pred)):
+        y_pred[price] = (y_pred[price] * (max_price - min_price)) + min_price
+    print("Predicciones:")
+    for pred in range(len(y_pred)):
+        print(f"Prediccion: {y_pred[pred]} Precio real: {precios_reales_test.iloc[pred]}")
+
 
 
 
